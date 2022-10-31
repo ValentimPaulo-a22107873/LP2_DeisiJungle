a261b07928bd0439b000c11868333daf418c38e2
Valentim Paulo <valentimspaulo@gmail.com>
2022-10-31 11:28:46 +0000

    Bibilotecas e packages adicionados

--- src/pt/ulusofona/lp2/deisiJungle/Player.java
+++ src/pt/ulusofona/lp2/deisiJungle/Player.java
@@ -2,19 +2,34 @@ package pt.ulusofona.lp2.deisiJungle;
 
 public class Player {
 
-    int id, specie, energy;
-
+    int id, specieId, energy;
     String name;
+
+
+
+    int getId(){
+        return id;
+    }
+
+    int getSpecieId(){
+        return specieId;
+    }
+
+    int getEnergy(){
+        return energy;
+    }
+
+    String getName(){
+        return name;
+    }
+
+    //FUNCOES PARA OBTER QUALQUER INFO DO PLAYER
+
+
+
 
     public Player() {
 
     }
-
-    
-
-
-
-
-
 
 }
