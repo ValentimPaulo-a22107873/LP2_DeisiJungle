2e632e96d457b19f6ee8f8d0bf73f9033399326d
pedroLopes-a22104722 <pedrosilvestrelopes@hotmail.com>
2022-10-31 11:23:28 +0000

    teste 1

--- src/pt/ulusofona/lp2/deisiJungle/GameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/GameManager.java
@@ -1,4 +1,19 @@
 package pt.ulusofona.lp2.deisiJungle;
 
 public class GameManager {
+
+
+
+    public GameManager() {
+
+
+    }
+
+    public String[] getPlayerInfo(int playerId){
+
+    }
+
+
+
+
 }
---
+++ src/pt/ulusofona/lp2/deisiJungle/Player.java
@@ -0,0 +1,20 @@
+package pt.ulusofona.lp2.deisiJungle;
+
+public class Player {
+
+    int id, specie, energy;
+
+    String name;
+
+    public Player() {
+
+    }
+
+    
+
+
+
+
+
+
+}
