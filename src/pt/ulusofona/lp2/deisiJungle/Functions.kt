a91093a53f11e03c4be0ca7058f2eae4c6ad735d
Valentim Paulo <valentimspaulo@gmail.com>
2023-01-04 18:56:09 +0000

    kotlin iniciado

---
+++ src/pt/ulusofona/lp2/deisiJungle/Functions.kt
@@ -0,0 +1,14 @@
+package pt.ulusofona.lp2.deisiJungle
+
+enum class CommandType {GET, POST}
+
+class Functions {
+
+    fun PLAYER_INFO(name : String) : String{
+        return "";
+    }
+
+
+
+
+}
